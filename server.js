359ae59a003a1eaa2b0be8428d2ed1af465d242f
joner1406 <jonerr1406@gmail.com>
2025-09-29 14:08:23 -0500

    Update server.js

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ process.on('unhandledRejection', (err) => {
 
 // Middleware CORS mejorado
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173','http://backend-inmanfinal-nf20.onrender.com');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5173','https://frontend-inmanfinal.onrender.com');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
